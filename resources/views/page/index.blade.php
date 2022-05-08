c61da0d60c5afd72445f9c9850a58cbbfc3314c3
a.krasnoperov <a.krasnoperov@ottoruss.ru>
2022-05-08 16:42:03 +0300

    update views

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -28,47 +28,7 @@
 
     @includeWhen($advantages, 'layouts.sections.advantages')
 
-    <!-- About Us-->
-    <section class="section section-inset-2 bg-default text-md-left">
-        <div class="container">
-            <div class="row row-30 align-items-start justify-content-center">
-                <div class="col-md-12 col-lg-12">
-                    <div class="form__question_marin">
-                        <h4 class="wow fadeInRight decorate title">Дорогие друзья!</h4>
-                        <p class="offset-top-md-20 wow fadeInRight decorate" data-wow-delay=".2s">
-                        </p>
-                        <p class="offset-top-md-20 wow fadeInRight decorate" data-wow-delay=".2s"><b>С уважением, коллектив
-                                "Private Estate"</b></p>
-                        <div class="group-middle d-md-flex justify-content-md-start wow fadeInRight" data-wow-delay=".3s">
-                            <div class="form_question">
-                                <div>
-                                    <div class="form_info"><p>Задать вопрос</p></div>
-                                    <form action="{{ route('send.question') }}"
-                                          class="rd-form rd-mailform rd-form-inline rd-form-inline-2" method="post"
-                                          id="form__subscribe">
-                                        @csrf
-                                        <div class="form-wrap">
-                                            <input class="form-input" id="subscribe-form-2-email" type="text" name="name"
-                                                   autocomplete="off" placeholder="Имя" required=""/>
-                                        </div>
-                                        <div class="form-wrap">
-                                            <input class="form-input" id="subscribe-form-2-email" type="text" name="phone"
-                                                   autocomplete="off" placeholder="Телефон" required=""/>
-                                        </div>
-                                        <div class="form-button submit">
-                                            <button class="button button-sm button-secondary" type="submit">
-                                                Задать вопрос
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @includeWhen($partners, 'layouts.forms.question_form')
 
     <section class="section-banner">
         <div class="container">
