d7497c9635036999fa18c79e8b4a328347bcad51
a.krasnoperov <a.krasnoperov@ottoruss.ru>
2022-05-08 16:50:58 +0300

    update views

--- resources/views/layouts/sections/product_color_theme.blade.php
+++ resources/views/layouts/sections/product_color_theme.blade.php
@@ -2,6 +2,9 @@
 <section class="section section-xl bg-default product-gallery">
     <div class="container">
         <div class="row row-30 isotope" data-lightgallery="group">
+            <div class="col-md-12">
+                <div class="our_service-name heading-3">{{ $product->colorTheme->name }}</div>
+            </div>
             @foreach ($product->colorTheme->ourServiceItems as $ourServiceItem)
             <div class="col-md-3 col-sm-3 col-lg-3 col-6">
                 <!-- Thumbnail Classic-->
